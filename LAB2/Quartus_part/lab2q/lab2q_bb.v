7e18ea368da34223569a6a0ea3b4444e4d671342
kevin8075 <kevin.parent.legault@gmail.com>
2015-05-24 14:31:14 -0400

    Plateforme physique non compilé.

--- LAB2/Quartus_part/lab2q/lab2q_bb.v
+++ LAB2/Quartus_part/lab2q/lab2q_bb.v
@@ -1,8 +1,48 @@
 
 module lab2q (
 	clk_clk,
-	reset_reset_n);	
+	vga_out_CLK,
+	vga_out_HS,
+	vga_out_VS,
+	vga_out_BLANK,
+	vga_out_SYNC,
+	vga_out_R,
+	vga_out_G,
+	vga_out_B,
+	sram_addr,
+	sram_ba,
+	sram_cas_n,
+	sram_cke,
+	sram_cs_n,
+	sram_dq,
+	sram_dqm,
+	sram_ras_n,
+	sram_we_n,
+	reset_reset_n,
+	mouse_CLK,
+	mouse_DAT,
+	clk_sdram_clk);	
 
 	input		clk_clk;
+	output		vga_out_CLK;
+	output		vga_out_HS;
+	output		vga_out_VS;
+	output		vga_out_BLANK;
+	output		vga_out_SYNC;
+	output	[7:0]	vga_out_R;
+	output	[7:0]	vga_out_G;
+	output	[7:0]	vga_out_B;
+	output	[12:0]	sram_addr;
+	output	[1:0]	sram_ba;
+	output		sram_cas_n;
+	output		sram_cke;
+	output		sram_cs_n;
+	inout	[15:0]	sram_dq;
+	output	[1:0]	sram_dqm;
+	output		sram_ras_n;
+	output		sram_we_n;
 	input		reset_reset_n;
+	inout		mouse_CLK;
+	inout		mouse_DAT;
+	output		clk_sdram_clk;
 endmodule
--- LAB2/Quartus_part/lab2q/lab2q_inst.v
+++ LAB2/Quartus_part/lab2q/lab2q_inst.v
@@ -1,5 +1,25 @@
 	lab2q u0 (
-		.clk_clk       (<connected-to-clk_clk>),       //   clk.clk
-		.reset_reset_n (<connected-to-reset_reset_n>)  // reset.reset_n
+		.clk_clk       (<connected-to-clk_clk>),       //       clk.clk
+		.vga_out_CLK   (<connected-to-vga_out_CLK>),   //   vga_out.CLK
+		.vga_out_HS    (<connected-to-vga_out_HS>),    //          .HS
+		.vga_out_VS    (<connected-to-vga_out_VS>),    //          .VS
+		.vga_out_BLANK (<connected-to-vga_out_BLANK>), //          .BLANK
+		.vga_out_SYNC  (<connected-to-vga_out_SYNC>),  //          .SYNC
+		.vga_out_R     (<connected-to-vga_out_R>),     //          .R
+		.vga_out_G     (<connected-to-vga_out_G>),     //          .G
+		.vga_out_B     (<connected-to-vga_out_B>),     //          .B
+		.sram_addr     (<connected-to-sram_addr>),     //      sram.addr
+		.sram_ba       (<connected-to-sram_ba>),       //          .ba
+		.sram_cas_n    (<connected-to-sram_cas_n>),    //          .cas_n
+		.sram_cke      (<connected-to-sram_cke>),      //          .cke
+		.sram_cs_n     (<connected-to-sram_cs_n>),     //          .cs_n
+		.sram_dq       (<connected-to-sram_dq>),       //          .dq
+		.sram_dqm      (<connected-to-sram_dqm>),      //          .dqm
+		.sram_ras_n    (<connected-to-sram_ras_n>),    //          .ras_n
+		.sram_we_n     (<connected-to-sram_we_n>),     //          .we_n
+		.reset_reset_n (<connected-to-reset_reset_n>), //     reset.reset_n
+		.mouse_CLK     (<connected-to-mouse_CLK>),     //     mouse.CLK
+		.mouse_DAT     (<connected-to-mouse_DAT>),     //          .DAT
+		.clk_sdram_clk (<connected-to-clk_sdram_clk>)  // clk_sdram.clk
 	);
 
